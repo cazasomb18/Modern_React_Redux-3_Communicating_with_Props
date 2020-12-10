7e5c850ef87e1262b6b6869c677c944c56542fc4
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-10 10:17:26 -0600

    repo created, files set up

---
+++ js/app.js
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//SECTION 3 - COMMUNICATING WITH PROPS
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Component Nesting 
+	//- a component can be shown INSIDE of another
+//Component Reusability 
+	//- We want to make components that can be easily reused throughout our application
+//Component Configuration 
+	//- We should be able to configure a component when its created
+
+
+//Semantic UI - semantic-ui.com
+	//open source css styling framework
+
+	//To install
+		//semantic ui cdn
+			//<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
+			//place this link ^^^ inside the public html file, into our head tag
